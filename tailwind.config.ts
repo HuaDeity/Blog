7aac67525ffcf45a6a9691b6e6a82093846f3df2
HuaDeity <hello@huadeity.com>
2024-02-20 01:31:24 +0800

    Test

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
-
-export default {
-  content: ["./hugo_stats.json"],
-  plugins: [
-    typography(),
-  ],
-} satisfies Config;
